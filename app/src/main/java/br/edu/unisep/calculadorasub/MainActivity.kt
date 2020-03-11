2ec29f0db71ca0646407acc134c7b1b1fa20d4a2
João Pedro <joao.cordeirocarvalho@gmail.com>
2020-03-11 09:51:34 -0300

    exemplo finalizado

---
+++ app/src/main/java/br/edu/unisep/calculadorasub/MainActivity.kt
@@ -0,0 +1,52 @@
+package br.edu.unisep.calculadorasub
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.text.DecimalFormat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setupEvents()
+    }
+
+    private fun setupEvents() {
+        buttonSubG1.setOnClickListener {
+            calculateSub(1)
+        }
+
+        buttonSubG2.setOnClickListener {
+            calculateSub(2)
+        }
+
+        buttonClear.setOnClickListener {
+            clear()
+        }
+    }
+
+    private fun calculateSub(option: Int) {
+        val grateKeep = editTextGradeKeep.text.toString().toDouble()
+
+        val gradeSub = if (option == 1) {
+            (21 - 2 * grateKeep)
+        } else {
+            (21 -  grateKeep) / 2
+        }
+
+        val df = DecimalFormat("#0.0")
+        textViewResult.text = df.format(gradeSub)
+
+        groupResult.visibility = View.VISIBLE
+    }
+
+    private fun clear() {
+        groupResult.visibility = View.INVISIBLE
+        editTextGradeKeep.text.clear()
+        editTextGradeKeep.requestFocus()
+    }
+}
